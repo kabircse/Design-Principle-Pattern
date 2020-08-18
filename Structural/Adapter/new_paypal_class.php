fd6d240f715049253dda103a0eb893a5fa29eee3
Kabir Hossain <kabir.cse10@gmail.com>
2020-08-18 16:58:53 +0600

    Update new_paypal_class.php

--- Structural/Adapter/new_paypal_class.php
+++ Structural/Adapter/new_paypal_class.php
@@ -2,31 +2,29 @@
 /** New paypal class rename a method name, sendPayment to payAmount
   * for matching this incompatibility with my existing system, just I will use an adapter class
 */
-class PayPal {
-     
+class PayPal {     
     public function __construct() {
         // Your Code here //
-    }
-     
-    public function payAmount($amount) {
+    }     
+    public function payAmount($amount) {   // payAmount() -> pay().
         // Paying via Paypal //
         echo "Paying via PayPal: ". $amount;
     }
 }
- 
- 
-class paypalAdapter{
-     
-    private $paypal;
- 
+
+/* Now PayPal library changed payAmount() method to pay().
+ But my large system uses previous payAmount() method, now I do not want to change the previous payAmount() method to my system.
+ So, I used paypalAdapter to adapting new library.
+ */ 
+class paypalAdapter{     
+    private $paypal; 
     public function __construct(PayPal $paypal) {
         $this->paypal = $paypal;
-    }
-     
-    public function pay($amount) {
-        $this->paypal->payAmount($amount);
+    }     
+    public function payAmount($amount) {
+        $this->paypal->pay($amount);
     }
 }
 // Client Code
 $paypal = new paypalAdapter(new PayPal());
-echo $paypal->pay('2629');
+echo $paypal->payAmount('2629');
