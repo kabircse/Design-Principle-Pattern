f6abe60bfc63af6de2d829d2f63950356b550239
Kabir Hossain <kabir.cse10@gmail.com>
2020-08-20 14:27:04 +0600

    Create CustomerListLogger.php

---
+++ Behavioral/Observer/CustomerListLogger.php
@@ -0,0 +1,25 @@
+<?php
+interface Observer {
+  function onChanged($sender, $args);
+}
+interface Observable {
+  function addObserver($observer);
+}
+class CustomerList implements Observable {
+  private $_observers = array();
+  public function addCustomer($name) {
+    foreach($this->_observers as $obs)
+      $obs->onChanged($this, $name);
+  }
+  public function addObserver($observer) {
+    $this->_observers []= $observer;
+  }
+}
+class CustomerListLogger implements Observer {
+  public function onChanged($sender, $args) {
+    echo( "'$args' Customer has been added to the list \n" );
+  }
+}
+$ul = new UserList();
+$ul->addObserver( new CustomerListLogger() );
+$ul->addCustomer( "Jack" );
