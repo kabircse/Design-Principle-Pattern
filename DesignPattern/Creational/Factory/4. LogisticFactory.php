d8e7fe6ecdb9c46b59b6ad7625020802e9aa76a2
Kabir Hossain <kabir.cse10@gmail.com>
2020-08-25 12:56:16 +0600

    Update 4. LogisticFactory.php

--- DesignPattern/Creational/Factory/4. LogisticFactory.php
+++ DesignPattern/Creational/Factory/4. LogisticFactory.php
@@ -2,16 +2,13 @@
 interface Transport {    
     fun deliver()
 }
-
 abstract class Logistic {
     abstract fun createTransport(): Transport
-
     fun deliver() {
         val transport: Transport = createTransport()
         transport.deliver()
     }
 }
-
 
 class SeaLogistic : Logistic() {
     override fun createTransport(): Transport {
@@ -36,30 +33,12 @@ class Truck : Transport {
     }
 }
 
-class FactoryMethodTest {
-  fun main() {
-      // Creating Ship using SeaLogistic
-      for(i in 1..5) {
-          val logistic = SeaLogistic()
-          logistic.deliver()
-      }
+// Creating Ship using SeaLogistic
+  val logistic = SeaLogistic();
+  logistic.deliver();
 
-      // Creating Truck using RoadLogistic
-      for(i in 1..5) {
-          val logistic = RoadLogistic()
-          logistic.deliver()
-      }
-  }
-}
+// Creating Truck using RoadLogistic
+  val logistic = RoadLogistic();
+  logistic.deliver();
 
-class AirLogistic: Logistic() {
-    override fun createTransport(): Transport {
-        return Plane()
-    }
-}
-
-class Plane : Transport {
-    override fun deliver() {
-        println("Delivering Plane with id: $this")
-    }
-}
+// AirLogistic->Plane other transport can exist.
